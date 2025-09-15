6858d9e1ddcff2926b59beb47c6e16b02c21a46f
Kimberlyn Pace <kimberlynpace@Kimberlyns-MacBook-Air.local>
2025-09-15 14:17:27 -0600

    Creates script containing draft documentation for example datasets referenced in tutorial vignette

---
+++ R/example_data.R
@@ -0,0 +1,33 @@
+#' Example Dataset 1: Balancing plot visits 
+#'
+#' Description 
+#'
+#' @format A dataframe with x rows and x variables 
+#' @source documentation for SWAN or maybe randomly generate stuff so it's simulated instead? 
+"example_balance_data"
+
+#' Example Dataset 2: Species composition data 
+#'
+#' Description 
+#'
+#' @format A dataframe with x rows and x variables 
+#' @source documentation for SWAN or maybe randomly generate stuff so it's simulated instead? 
+"woodland_data"
+
+#' Example Dataset 3: Environmental variables for species compositiond data 
+#'
+#' Description 
+#'
+#' @format A dataframe with x rows and x variables 
+#' @source documentation for SWAN or maybe randomly generate stuff so it's simulated instead? 
+"woodland_env"
+
+
+
+#library(readxl)
+#woodland_data <- read_excel("/Users/kimberlynpace/Desktop/permanovaTools/data/woodland_data.xlsx")
+#woodland_env<- read_excel("/Users/kimberlynpace/Desktop/permanovaTools/data/woodland_env.xlsx")
+#example_balance_data <- read_excel("/Users/kimberlynpace/Desktop/permanovaTools/data/example_balance_data.xlsx")
+#save(woodland_data, file = "/Users/kimberlynpace/Desktop/permanovaTools/data/woodland_data.rda")
+#save(woodland_env, file = "/Users/kimberlynpace/Desktop/permanovaTools/data/woodland_env.rda")
+#save(example_balance_data, file = "/Users/kimberlynpace/Desktop/permanovaTools/data/example_balance_data.rda")
